7406bc28839e53fb889352fd4eac017fe77364a9
Ntahorg <jakson.flowers@ymail.com>
2013-10-04 02:01:35 -0700

    Added Druid of the Fang in wailing caverns.

---
+++ Required/scripts/creatures/Druid of the Fang.vb
@@ -0,0 +1,67 @@
+Imports System
+Imports System.Threading
+Imports WorldServer
+
+Namespace Scripts
+    Public Class CreatureAI
+        Inherits BossAI
+        'AI TO DO: Implement slumber, add healing correctly to the mob.
+        Private Const AI_UPDATE As Integer = 1000
+        'Private Const SLUMBER_CD As Integer = 10000 - Unable to implement this as for the time being due to threat issues in the core.
+        Private Const Healing_Touch_CD As Integer = 20000
+        Private Const Serpent_Form_CD As Integer = 40000
+        Private Const Lightning_Bolt_CD As Integer = 6000
+
+
+        ' Private Const Slumber_Spell As Integer = 8040
+        Private Const Healing_Spell As Integer = 23381
+        Private Const Spell_Serpent_Form As Integer = 8041 'Not sure how this will work. 
+        Private Const Spell_Lightning_Bolt As Integer = 9532
+        Public NextWaypoint As Integer = 0
+        Public NextLightningBolt As Integer = 0
+        Public NextHealingTouch As Integer = 0
+        'public NextSlumber as integer = 0
+        Public NextSerpentForm As Integer = 0 'This should never be re-casted.
+        Public CurrentWaypoint As Integer = 0
+
+        Public Sub New(ByRef Creature As CreatureObject)
+            MyBase.New(Creature)
+
+            AllowedMove = False
+            Creature.Flying = False
+
+            Creature.VisibleDistance = 700
+        End Sub
+        Public Overrides Sub OnThink()
+
+            NextLightningBolt -= AI_UPDATE
+            NextSerpentForm -= AI_UPDATE
+            NextHealingTouch -= AI_UPDATE
+            'NextSlumber -= AI_UPDATE
+
+            If NextLightningBolt <= 0 Then
+                NextLightningBolt = Lightning_Bolt_CD
+                aiCreature.CastSpell(Spell_Lightning_Bolt, aiTarget) 'Lightning bolt on current target.
+            End If
+        End Sub
+
+        Public Sub CastLightning()
+            For i As Integer = 0 To 3
+                Dim Target As BaseUnit = aiCreature
+                If Target Is Nothing Then Exit Sub
+
+                aiCreature.CastSpell(Spell_Lightning_Bolt, aiTarget)
+            Next
+        End Sub
+
+        Public Overrides Sub OnHealthChange(ByVal Percent As Integer)
+            If Percent <= 30 Then
+                Try
+                    aiCreature.CastSpellOnSelf(Spell_Serpent_Form)
+                Catch ex As Exception
+                    aiCreature.SendChatMessage("I have failed to cast Serpent Form. This is a problem. Please report this to the developers.", ChatMsg.CHAT_MSG_MONSTER_YELL, LANGUAGES.LANG_UNIVERSAL)
+                End Try
+            End If
+        End Sub
+    End Class
+End Namespace
