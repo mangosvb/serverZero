b4458b984a3ee6d8974ac9996730e8528a6b1999
Haruhi <mfrink@baldwin-telecom.net>
2013-10-25 10:24:29 -0500

    Moving Zombie Chow into the new location for Creature Scripts. More to go here.

--- Server/WorldServer/Scripts/Creatures/ZombieChow.vb
+++ Server/WorldServer/Scripts/Creatures/ZombieChow.vb
@@ -1,6 +1,7 @@
 Imports System
 Imports System.Threading
 Imports mangosVB.WorldServer
+
 'Basically, this AI is kitable and if the AI hits Gluth, it heals her for 5% of her HP (50,000 in this case.). Since we can't really do it that way, it has a set waypoint.
 Namespace Scripts
     Public Class CreatureAI
@@ -22,6 +23,7 @@ Namespace Scripts
             Creature.Flying = False
             Creature.VisibleDistance = 700
         End Sub
+
         Public Overrides Sub OnThink()
             NextInfectedWound -= AI_UPDATE
 
@@ -40,7 +42,7 @@ Namespace Scripts
         End Sub
 		
         Public Sub HealGluth(ByRef NPC_Gluth As CreatureObject, ByRef Zombie_Chow As CreatureObject)
-            Dim Waypoint1 As New coOrds
+            Dim Waypoint1 As New coords
             With Waypoint1
                 .X = 3304.919922
                 .Y = 3139.149902
@@ -54,7 +56,7 @@ Namespace Scripts
             End If
         End Sub
 
-        Private Structure coOrds
+        Private Structure coords
             Dim X As Double
             Dim Y As Double
             Dim Z As Double
