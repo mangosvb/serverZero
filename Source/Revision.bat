941cdb51a4b4a529a809625d1ccb15710c007ee5
Romfos <Romfos@tut.by>
2020-09-26 22:11:11 +0300

    Remove unusable files

--- Source/Revision.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" "%CD%" Common\Revision.tmp.vb RealmServer\Revision.vb
-"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" "%CD%" Common\Revision.tmp.vb WorldCluster\Revision.vb
-"C:\Program Files\TortoiseSVN\bin\SubWCRev.exe" "%CD%" Common\Revision.tmp.vb WorldServer\Revision.vb
