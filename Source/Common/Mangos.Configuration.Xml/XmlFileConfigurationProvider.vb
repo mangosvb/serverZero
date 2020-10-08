96924cd6fc0e256bc0472d5a400e04b304f35d3d
Romfos <Romfos@tut.by>
2020-10-08 23:56:41 +0300

    Added Mangos.Configuration.Xml

---
+++ Source/Common/Mangos.Configuration.Xml/XmlFileConfigurationProvider.vb
@@ -0,0 +1,20 @@
+﻿Imports System.IO
+Imports System.Xml.Serialization
+
+Public Class XmlFileConfigurationProvider(Of T)
+    Implements IConfigurationProvider(Of T)
+
+    Private ReadOnly _filePath As String
+
+    Sub New(filePath As String)
+        _filePath = filePath
+    End Sub
+
+    Public Function GetConfigurationAsync() As Task(Of T) Implements IConfigurationProvider(Of T).GetConfigurationAsync
+        Using streamReader As New StreamReader(_filePath)
+            Dim xmlSerializer = New XmlSerializer(GetType(T))
+            Dim configuration As T = xmlSerializer.Deserialize(streamReader)
+            Return Task.FromResult(configuration)
+        End Using
+    End Function
+End Class
